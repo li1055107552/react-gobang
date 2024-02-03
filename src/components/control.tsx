a42d51e3f38baed26650bed1f6fe58a2d73034b4
li1055107552 <li1055107552@163.com>
2024-02-03 09:23:26 +0800

    useRef to achieve review

--- src/components/control.tsx
+++ src/components/control.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { row, col } from "./Board"
 
 export default function Control(
@@ -13,22 +13,32 @@ export default function Control(
         setHistort
     }: any) {
 
-    let [timerId, setTimerId] = useState<null | NodeJS.Timeout>(null);
-    let [timerStatus, setTimerStatus] = useState<number>(0);
+
+    let stepTmp = useRef(step)
+    let isAutoReview = useRef(false);
+    let delayTime = useRef(0);
+    // let [isAutoReview, setIsAutoReview] = useState<boolean>(true);
+    // let [delayTime, setDelayTime] = useState<number>(0);
 
     const restart = () => {
         setStatus(null)
         setStep(0)
         setSquares(Array(row * col).fill(null));
         setHistort(Array(0));
+        stepTmp.current = 0
+        isAutoReview.current = false
+        delayTime.current = 0
     }
 
     const review = () => {
         setStatus("review")
+        stepTmp.current = step
     }
 
     const toStart = () => {
         setStep(0)
+        isAutoReview.current = false
+        stepTmp.current = 0
         setSquares(Array(row * col).fill(null))
     }
 
@@ -37,7 +47,7 @@ export default function Control(
         let index: number = 0;
 
         if (status === "review") {
-            index = history[step - 1]
+            index = history[stepTmp.current - 1]
         }
         else if (status === "continue") {
             index = history.pop() as number
@@ -47,13 +57,19 @@ export default function Control(
             setStatus("continue")
         }
 
-        setStep(step - 1)
-        nextSquares[index] = null
-        setSquares(nextSquares)
+        setStep(stepTmp.current - 1)
+        stepTmp.current = stepTmp.current - 1
+
+        // nextSquares[index] = null
+        setSquares((pre: any[]) => {
+            pre[index] = null
+            return pre.slice()
+        })
     }
 
     const stop = () => {
         console.log("stop");
+        isAutoReview.current = false
     }
 
     const next = () => {
@@ -61,10 +77,13 @@ export default function Control(
         let nextSquares = squares.slice()
         nextSquares[index] = step % 2 === 0 ? "black" : "white"
         setStep(step + 1)
+        stepTmp.current = step + 1
         setSquares(nextSquares)
     }
 
     const toEnd = () => {
+        stepTmp.current = step
+
         let steptmp = step
         let nextSquares = squares.slice()
         for (steptmp < history.length; steptmp < history.length; steptmp++) {
@@ -72,57 +91,74 @@ export default function Control(
             nextSquares[index] = steptmp % 2 === 0 ? "black" : "white"
         }
         setStep(steptmp)
+        stepTmp.current = steptmp
+
         setSquares(nextSquares)
     }
 
     const x1 = () => {
-        setTimerStatus(1000)
+        let time = delayTime.current
+        delayTime.current = 1000 / 1
+
+        if (!isAutoReview.current) {
+            setTimeout(() => {
+                auto()
+            }, time);
+        }
     }
     const x2 = () => {
-        setTimerStatus(500)
-    }
+        let time = delayTime.current
+        delayTime.current = 1000 / 2
 
-
-    const auto = (time: number) => {
-
-        return setInterval(() => {
-
-            let stepTmp = step++
-            if (stepTmp < history.length) {
-                let index = history[stepTmp]
-                setSquares((pre: any[]) => {
-                    pre[index] = stepTmp % 2 === 0 ? "black" : "white"
-                    return pre.slice()
-                })
-            }
-            else {
-                clearInterval(timerId as NodeJS.Timeout)
-                setTimerId(null)
-            }
-        }, time)
-    }
-
-    useEffect(() => {
-        if (timerId) {
-            clearInterval(timerId)
-            setTimerId(null)
+        if (!isAutoReview.current) {
+            setTimeout(() => {
+                auto()
+            }, 1000);
         }
 
-        let Interval = auto(timerStatus)
-        setTimerId(Interval)
+    }
 
-        return () => {
-            clearInterval(Interval);
-        };
 
-    }, [timerStatus])
+    const auto = async () => {
+        console.log("auto run");
+
+        isAutoReview.current = true
+
+        while (stepTmp.current < history.length) {
+            console.log(isAutoReview.current);
+            if (isAutoReview.current) { // 判断是否被打断 或 更改复盘条件
+                console.log(stepTmp.current);
+                await new Promise<void>((resolve, reject) => {
+
+                    let index = history[stepTmp.current]
+                    setSquares((pre: any[]) => {
+                        pre[index] = stepTmp.current % 2 === 0 ? "black" : "white"
+                        return pre.slice()
+                    })
+
+                    setTimeout(() => {
+                        isAutoReview.current && stepTmp.current++
+                        resolve()
+                    }, delayTime.current)
+                })
+
+            }
+            else {
+                setStep(stepTmp.current)
+                break
+            }
+        }
+        setStep(stepTmp.current)
+        isAutoReview.current = false
+    }
+
 
     const btns = [
         { action: "toStart", callback: toStart, text: "II<", show: () => status === "review" },
-        { action: "back", callback: back, text: "<", show: () => step !== 0 },
+        { action: "back", callback: back, text: "<", show: () => stepTmp.current !== 0 },
         { action: "stop", callback: stop, text: "II", show: () => status === "review" },
-        { action: "next", callback: next, text: ">", show: () => step < history.length },
-        { action: "toEnd", callback: toEnd, text: ">II", show: () => status === "review" && step < history.length },
+        { action: "next", callback: next, text: ">", show: () => stepTmp.current < history.length },
+        { action: "toEnd", callback: toEnd, text: ">II", show: () => status === "review" && stepTmp.current < history.length },
         { action: "review", callback: review, text: "复盘", show: () => status === "end" },
         { action: "x1", callback: x1, text: "x1", show: () => status === "review" },
         { action: "x2", callback: x2, text: "x2", show: () => status === "review" }
